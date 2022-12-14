e4253c101cabc969721623195a2ec37e65e6852a
Yonatan kps <yonatank@cyncsecure.com>
2022-12-14 14:03:13 +0200

    add login page validation for email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { HttpClientModule } from '@angular/common/http';
+import { NgToastModule } from 'ng-angular-popup'
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { TodoComponent } from './components/todo/todo.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent, LoginComponent, SignupComponent],
+  declarations: [AppComponent, TodoComponent, LoginComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
-    AppRoutingModule,
+    HttpClientModule,
+    NgToastModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { NgToastService } from 'ng-angular-popup';
+import { tap } from 'rxjs';
+
+import { MyDataService } from '../../services/my-data.service';
+import ValidateForm from '../helpers/validateform';
 
 @Component({
   selector: 'app-login',
@@ -6,16 +20,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usersData: any;
+  usersData$: any;
   type: string = 'password';
   isText: boolean = false;
   eyeIcon: string = 'fa-eye-slash';
-  constructor() {}
+  loginForm!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private fb: FormBuilder,
+    private myDataService: MyDataService,
+    private http: HttpClient,
+    private router: Router,
+    private toast: NgToastService
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+
+    this.usersData$ = this.myDataService
+      .getData()
+      .pipe(tap((data) => (this.usersData = data)));
+  }
 
   hideShowPass() {
     this.isText = !this.isText;
     this.isText ? (this.eyeIcon = 'fa-eye') : (this.eyeIcon = 'fa-eye-slash');
     this.isText ? (this.type = 'text') : (this.type = 'password');
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.http
+        .get<any>('https://jsonplaceholder.typicode.com/users')
+        .subscribe((res) => {
+          const user = res.find(
+            (user: any) => user.email === this.loginForm.value.email
+          );
+          if (user) {
+            // alert('User is found');
+            this.toast.success({detail: "SUCCESS", summary: "Successfully logged in!", duration: 3000});
+            this.loginForm.reset();
+            this.router.navigate(['todo']);
+          } else {
+            this.toast.error({detail: "ERROR", summary: "User not found", duration: 3000});
+          }
+          (err: any) => {
+            this.toast.error({detail: "ERROR", summary: "Something went wrong", duration: 3000});
+          };
+        });
+    } else {
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert('Your form is invalid please check again');
+    }
   }
 }
