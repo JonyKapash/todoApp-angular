7f3eaa0b0293902ea1d90f5425a83ef16bac0d80
Yonatan kps <yonatank@cyncsecure.com>
2022-12-14 20:23:13 +0200

    Add hide completed todos

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 import { TodoService } from '../../shared/todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styles: [],
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
   todos: any[] = [];
+  displayTodos: any[] = [];
+  hideCompletedTodos: boolean = false;
 
   constructor(private todoService: TodoService) {}
 
@@ -16,10 +19,11 @@ export class TodoComponent implements OnInit {
       .valueChanges({ idField: 'id' })
       .subscribe((item) => {
         this.todos = item.sort((a: any, b: any) => a.isDone - b.isDone);
+        this.setDisplayTodos();
       });
   }
 
-  onClick(titleInput: HTMLInputElement) {
+  onAdd(titleInput: HTMLInputElement) {
     if (titleInput.value) {
       this.todoService.addTodo(titleInput.value);
       titleInput.value = '';
@@ -32,5 +36,20 @@ export class TodoComponent implements OnInit {
 
   onDelete(id: string) {
     this.todoService.deleteTodoById(id);
+  }
+
+  getTodoCount() {
+    return this.todos.length;
+  }
+
+  toggleHideCompletedTodos() {
+    this.hideCompletedTodos = !this.hideCompletedTodos;
+    this.setDisplayTodos();
+  }
+
+  setDisplayTodos() {
+    this.displayTodos = this.hideCompletedTodos
+      ? this.todos.filter((todo) => !todo.isDone)
+      : this.todos;
   }
 }
